76efa76b210eb8ff2a13826050038349fa5c74fd
Khoa Bean <khoakomlem@gmail.com>
2026-04-19 19:20:59 +0700

    fix formatPrice USD to VND after currency migration

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatPrice(price: number): string {
-	return new Intl.NumberFormat("en-US", {
+	return new Intl.NumberFormat("vi-VN", {
 		style: "currency",
-		currency: "USD",
+		currency: "VND",
 	}).format(price)
 }
 
